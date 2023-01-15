a868f9a28cfd0fb1dc34d1085e6be3fea2ee6ce0
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-01-15 17:22:04 +0100

    add addFigurePlayer, addFigureComputer, backgroundColor and upgrade displayResult

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,48 @@ function printMessage(msg){
 	document.getElementById('messages').appendChild(div);
 }
 
+function addFigurePlayer(playFigure){
+	let div = document.createElement('div');
+	div.innerHTML = playFigure;
+	document.getElementById('playerField').appendChild(div);
+}
+
+function addFigureComputer(playFigure){
+	let div = document.createElement('div');
+	div.innerHTML = playFigure;
+	document.getElementById('computerField').appendChild(div);
+}
+
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
+	document.getElementById('playerField').innerHTML = '';
+	document.getElementById('computerField').innerHTML = '';
+}
+
+function backgroundColor(argPlayer, argComputer){
+	console.log(argPlayer);
+	let player = document.getElementById('playerField');
+	let computer = document.getElementById('computerField');
+
+	if (player.classList.contains('winGreen')){
+		player.classList.remove('winGreen');
+	} else if (player.classList.contains('loseRed')){
+		player.classList.remove('loseRed');
+	} else if (player.classList.contains('drawWhite')){
+		player.classList.remove('drawWhite');
+	}
+
+	if (computer.classList.contains('winGreen')){
+		computer.classList.remove('winGreen');
+	} else if (computer.classList.contains('loseRed')){
+		computer.classList.remove('loseRed');
+	} else if (computer.classList.contains('drawWhite')){
+		computer.classList.remove('drawWhite');
+	}
+
+	player.classList.add(argPlayer);
+	console.log(argComputer);
+	computer.classList.add(argComputer);
 }
 
 function getMoveName(argMoveId){
@@ -22,24 +62,31 @@ function getMoveName(argMoveId){
   }
   
   function displayResult(argComputerMove, argPlayerMove){
-	printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+	//printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
   
 	if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-	  printMessage('Ty wygrywasz!');
+	  //printMessage('Ty wygrywasz!');
+	  backgroundColor('winGreen','loseRed');
 	} else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
-		printMessage('Komputer wygrywa!');
+		//printMessage('Komputer wygrywa!');
+		backgroundColor('loseRed','winGreen');
 	} else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
-		printMessage('Komputer wygrywa!');
+		//printMessage('Komputer wygrywa!');
+		backgroundColor('loseRed','winGreen');
 	} else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-		printMessage('Ty wygrywasz!');
+		//printMessage('Ty wygrywasz!');
+		backgroundColor('winGreen','loseRed');
 	} else if(argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
-		printMessage('Komputer wygrywa!');
+		//printMessage('Komputer wygrywa!');
+		backgroundColor('loseRed','winGreen');
 	} else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-		printMessage('Ty wygrywasz');
+		//printMessage('Ty wygrywasz');
+		backgroundColor('winGreen','loseRed');
 	} else if(argComputerMove == argPlayerMove){
-		printMessage('Remis');
+		//printMessage('Remis');
+		backgroundColor('drawWhite','drawWhite')
 	}else {
-		printMessage('Nieznany ruch, brak rozstrzygnięcia');
+		//printMessage('Nieznany ruch, brak rozstrzygnięcia');
 	}
 	}
 
@@ -48,16 +95,22 @@ function playGame(playerInput){
 	let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 	console.log('Wylosowana liczba to: ' + randomNumber);
-
+	if(randomNumber == 1){
+		addFigureComputer('<i class="fa-solid fa-hand-back-fist"></i>');
+	} else if(randomNumber == 2){
+		addFigureComputer('<i class="fa-solid fa-hand"></i>');
+	} else {
+		addFigureComputer('<i class="fa-solid fa-hand-scissors"></i>');
+	}
 	let computerMove = getMoveName(randomNumber);
 
-	printMessage('Mój ruch to: ' + computerMove);
+	//printMessage('Mój ruch to: ' + computerMove);
 
 	console.log('Gracz wpisał: ' + playerInput);
 
 	let playerMove = getMoveName(playerInput);
 
-	printMessage('Twój ruch to: ' + playerMove);
+	//printMessage('Twój ruch to: ' + playerMove);
 
 	displayResult(computerMove,playerMove);
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 document.getElementById('play-rock').addEventListener('click', function(){
-    playGame(1);
+    playGame(1), addFigurePlayer('<i class="fa-solid fa-hand-back-fist"></i>');
   });
   document.getElementById('play-paper').addEventListener('click', function(){
-    playGame(2);
+    playGame(2), addFigurePlayer('<i class="fa-solid fa-hand"></i>');
   });
 
   document.getElementById('play-scissors').addEventListener('click', function(){
-    playGame(3);
+    playGame(3), addFigurePlayer('<i class="fa-solid fa-hand-scissors"></i>');
   });
