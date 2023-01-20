e2febb2dd3bb8d299f2b638dcd62fcdd6fa3a37b
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-01-20 18:03:50 +0100

    add prettier and config

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,23 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = '<h1>' + msg + '</h1>';
-	document.getElementById('messages').appendChild(div);
+function printMessage(msg) {
+  let div = document.createElement("div");
+  div.innerHTML = "<h1>" + msg + "</h1>";
+  document.getElementById("messages").appendChild(div);
 }
 
-function printResult(resultPlayer, resultComputer){
-	let tbody = document.getElementById('game-history');
-	let newRow = tbody.insertRow();
-	let cellPlayer = newRow.insertCell();
-	let cellComputer = newRow.insertCell();
-	let textPlayer = document.createTextNode(resultPlayer);
-	let textComputer = document.createTextNode(resultComputer);
+function printResult(resultPlayer, resultComputer) {
+  let tbody = document.getElementById("game-history");
+  let newRow = tbody.insertRow();
+  let cellPlayer = newRow.insertCell();
+  let cellComputer = newRow.insertCell();
+  let textPlayer = document.createTextNode(resultPlayer);
+  let textComputer = document.createTextNode(resultComputer);
 
-	cellPlayer.appendChild(textPlayer);
-	cellComputer.appendChild(textComputer);
+  cellPlayer.appendChild(textPlayer);
+  cellComputer.appendChild(textComputer);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-	document.getElementById('playerField').innerHTML = '';
-	document.getElementById('computerField').innerHTML = '';
+function clearMessages() {
+  document.getElementById("messages").innerHTML = "";
+  document.getElementById("playerField").innerHTML = "";
+  document.getElementById("computerField").innerHTML = "";
 }
